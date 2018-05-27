aa90365546b28212657a29e66cec1c18caae6827
Per Christian Henden <perchrh-gh@pvv.org>
2018-05-27 21:11:00 +0200

    Add parsing of both sdn and consolidated list

--- reader.py
+++ reader.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
 
 import sdn
-rootObject = sdn.parse('cons_advanced.xml', silence=True)
+consolidated_list = sdn.parse('cons_advanced.xml', silence=True)
+sdn_list = sdn.parse('sdn_advanced.xml', silence=True)
 
-sanctioned_parties = rootObject.DistinctParties.get_DistinctParty()
 
-number_of_parties = len(sanctioned_parties)
+for sanction_list in [consolidated_list, sdn_list]:
+    sanctioned_parties = sanction_list.DistinctParties.get_DistinctParty()
 
-print("Number of sanctioned parties in OFAC consolidated sanctions list is", number_of_parties)
+    number_of_parties = len(sanctioned_parties)
 
-# test av party 0
+    print("Number of sanctioned parties in OFAC list is", number_of_parties)
 
-for party in sanctioned_parties:
-    for profile in party.Profile:
-        if profile.PartySubTypeID == 4:
-            for identity in profile.Identity:
-                for alias in identity.Alias:
-                    for name in alias.DocumentedName:
-                        for namepart in name.DocumentedNamePart:
-                            value = namepart.NamePartValue
-                            namevalue = value.get_valueOf_()
-                            print(namevalue)
+    # print person names in list 
+
+    for party in sanctioned_parties:
+        for profile in party.Profile:
+            if profile.PartySubTypeID == 4: # consider only persons for now
+                for identity in profile.Identity:
+                    for alias in identity.Alias:
+                        for name in alias.DocumentedName:
+                            parts = []
+                            for namepart in name.DocumentedNamePart:
+                                namepart_value = namepart.NamePartValue
+                                namevalue = namepart_value.get_valueOf_()
+                                parts.append(namevalue)
+                            print(parts)
 
