83dada815bf4c8249c1d7ea1df9ca93d2721ec43
Per Henden <per.christian.henden@eika.no>
2018-05-28 10:43:20 +0200

    Filter name list on actual names and their latin spelling

--- reader.py
+++ reader.py
@@ -12,19 +12,20 @@ for sanction_list in [consolidated_list, sdn_list]:
 
     print("Number of sanctioned parties in OFAC list is", number_of_parties)
 
-    # print person names in list 
+    # print person names in list
 
     for party in sanctioned_parties:
         for profile in party.Profile:
             if profile.PartySubTypeID == 4: # consider only persons for now
                 for identity in profile.Identity:
                     for alias in identity.Alias:
-                        for name in alias.DocumentedName:
-                            parts = []
-                            for namepart in name.DocumentedNamePart:
-                                namepart_value = namepart.NamePartValue
-                                namevalue = namepart_value.get_valueOf_()
-                                parts.append(namevalue)
-                            print(party.FixedRef, parts)
+                        if alias.AliasTypeID == 1403: # consider names only
+                            for name in alias.DocumentedName:
+                                if name.DocNameStatusID == 1: # primary latin alphabet
+                                    parts = []
+                                    for namepart in name.DocumentedNamePart:
+                                        namepart_value = namepart.NamePartValue
+                                        namevalue = namepart_value.get_valueOf_()
+                                        parts.append(namevalue)
+                                    print(party.FixedRef, parts)
                 print("") # new line
-
