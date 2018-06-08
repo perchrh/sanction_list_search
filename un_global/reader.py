fa3d031baa94e8812db5edfa7be907c2a1595e3f
Per Henden <per.christian.henden@eika.no>
2018-06-08 15:15:47 +0200

    Improve alias collection

--- un_global/reader.py
+++ un_global/reader.py
@@ -8,25 +8,37 @@ individuals = sanctions.INDIVIDUALS.INDIVIDUAL
 
 for entity in entities:
     aliases = []
-    fixedRef = entity.REFERENCE_NUMBER
+    fixedRef = entity.REFERENCE_NUMBER.strip()
     if entity.DELISTED_ON:
         continue # don't include delisted entries
+
     aliases.append(entity.FIRST_NAME)
     for alias in entity.ENTITY_ALIAS:
+        if alias.QUALITY == 'Low':
+            continue #skip these for now
+            
         name = alias.ALIAS_NAME
         if name:
             aliases.append(name)
     print(fixedRef, aliases)
 
 for individual in individuals:
-    aliases = []
-    fixedRef = individual.REFERENCE_NUMBER
+    aliases = set()
+    fixedRef = individual.REFERENCE_NUMBER.strip()
     if individual.DELISTED_ON:
         continue # don't include delisted entries
+
     name_parts = [individual.FIRST_NAME , individual.SECOND_NAME, individual.THIRD_NAME, individual.FOURTH_NAME]
-    aliases.append(" ".join([part for part in name_parts if part]))
+    name = " ".join([part for part in name_parts if part])
+    aliases.add(name)
     for alias in individual.INDIVIDUAL_ALIAS:
-        name = alias.ALIAS_NAME
-        if name:
-            aliases.append(name)
+        if alias.QUALITY == 'Low':
+            continue #skip these for now
+
+        alias_names = alias.ALIAS_NAME
+        alias_names = " ".join(alias_names.split()) #remove white-space and linebreaks
+        if alias_names:
+            alias_list = alias_names.split(";")
+            for item in alias_list: aliases.add(item)
+
     print(fixedRef, aliases)
