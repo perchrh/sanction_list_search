552221f596ce7efaf103ed415dc1a2dace66d1e9
Per Christian Henden <perchrh-gh@pvv.org>
2018-06-20 21:27:53 +0200

    Read birth dates exact values (i.e. not ranges) too

--- ofac/reader.py
+++ ofac/reader.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python3
 
+from _datetime import datetime
+def datePeriodPrinter(DatePeriod):
+    start_date_from = create_single_date(DatePeriod.Start.From)
+    start_date_to = create_single_date(DatePeriod.Start.To)
+
+    end_date_from = create_single_date(DatePeriod.End.From)
+    end_date_to = create_single_date(DatePeriod.End.To)
+
+    if start_date_from == start_date_to:
+        if end_date_from == end_date_to:
+            if start_date_from == end_date_from:
+                return start_date_from # all values equal, this is a single exact date
+            else:
+                # TODO ranges not supported
+                return None #(start_date_from, end_date_from) # equal pairs
+    else:
+        # TODO ranges not supported
+        # what is this case supposed to represent? that the whole year is included?
+        #values = [start_date_from, start_date_to, end_date_to, end_date_from]
+        #return tuple(values)
+        return None
+
+
+def create_single_date(date):
+    month = date.Month.valueOf_
+    day = date.Day.valueOf_
+    if len(month) == 1:
+        month = "0" + month
+    if len(day) == 1:
+        day = "0" + day
+
+    return datetime.strptime("{} {} {}".format(date.Year.valueOf_, month, day),
+                             '%Y %m %d')
+
+
 import sdn
 
 consolidated_list = sdn.parse('cons_advanced.xml', silence=True)
@@ -7,6 +42,8 @@ sdn_list = sdn.parse('sdn_advanced.xml', silence=True)
 
 sources = [consolidated_list, sdn_list]
 for sanction_list in sources:
+    multiple_date_counter = 0
+
     sanctioned_parties = sanction_list.DistinctParties.get_DistinctParty()
 
     number_of_parties = len(sanctioned_parties)
@@ -17,19 +54,39 @@ for sanction_list in sources:
 
     for party in sanctioned_parties:
         name_aliases = []
+        date_aliases = []
         for profile in party.Profile:
             if profile.PartySubTypeID == 4:  # consider only persons for now
+                for feature in profile.Feature:
+                    if feature.FeatureTypeID == 8: # birthdate
+                        for version in feature.FeatureVersion:
+                            # there is currently never more than version in the list, and its unclear how versions are to be marked current and outdated
+                            if version.ReliabilityID == 1561: #1561 means it's been proven false, so skip it
+                                continue
+
+                            for period in version.DatePeriod:
+                                date_aliases.append(period)
                 for identity in profile.Identity:
                     for alias in identity.Alias:
                         if alias.LowQuality == False:
                             for name in alias.DocumentedName:
-                                if name.DocNameStatusID == 1:  # primary latin alphabet
-                                    parts = []
-                                    for namepart in name.DocumentedNamePart:
-                                        namepart_value = namepart.NamePartValue
-                                        namevalue = namepart_value.get_valueOf_()
+                                parts = []
+                                for namepart in name.DocumentedNamePart:
+                                    namepart_value = namepart.NamePartValue # NamePartValue.ScriptID
+                                    if namepart_value.ScriptID == 215: #latin
+                                        namevalue = namepart_value.valueOf_
                                         parts.append(namevalue)
-                                    if parts:
-                                        name_aliases.append(parts)
+                                if parts:
+                                    name_aliases.append(parts)
         if name_aliases:
-            print(party.FixedRef, name_aliases)
+            dates = [datePeriodPrinter(d) for d in date_aliases]
+            date_prints = [d.strftime("%Y-%m-%d") for d in dates if d]
+            if len(date_prints) > 1:
+                multiple_date_counter = multiple_date_counter + 1
+                print("Multiple dates here")
+
+            print(party.FixedRef, name_aliases, date_prints)
+
+    print("Got", multiple_date_counter, "entries with multiple dates", 100.0*multiple_date_counter/len(sanctioned_parties), "%")
+
+
--- un_global/reader.py
+++ un_global/reader.py
@@ -8,12 +8,13 @@ entities = sanctions.ENTITIES.ENTITY
 individuals = sanctions.INDIVIDUALS.INDIVIDUAL
 
 for entity in entities:
-    aliases = []
-    fixedRef = entity.REFERENCE_NUMBER.strip()
     if entity.DELISTED_ON:
         continue  # don't include delisted entries
 
-    aliases.append(entity.FIRST_NAME.strip())
+    fixedRef = entity.REFERENCE_NUMBER.strip()
+
+    name_aliases = []
+    name_aliases.append(" ".join(entity.FIRST_NAME.split()).strip()) # companies have only first names in list
     for alias in entity.ENTITY_ALIAS:
         if alias.QUALITY == 'Low':
             continue  # skip these for now
@@ -21,19 +22,27 @@ for entity in entities:
         name = alias.ALIAS_NAME
         name = " ".join(name.split())  # remove white-space and linebreaks
         if name:
-            aliases.append(name)
-    print(fixedRef, aliases)
+            name_aliases.append(name)
+    print(fixedRef, name_aliases)
 
+multiple_dates = 0
 for individual in individuals:
-    aliases = set()
-    fixedRef = individual.REFERENCE_NUMBER.strip()
     if individual.DELISTED_ON:
         continue  # don't include delisted entries
+
+    fixedRef = individual.REFERENCE_NUMBER.strip()
+    name_aliases = set()
 
     name_parts = [individual.FIRST_NAME, individual.SECOND_NAME, individual.THIRD_NAME, individual.FOURTH_NAME]
     name_parts = [" ".join(name.split()) for name in name_parts if name]  # remove white-space and linebreaks
     name = " ".join(name_parts)
-    aliases.add(name)
+    name_aliases.add(name)
+
+    date_aliases = set()
+    for date_of_birth in individual.INDIVIDUAL_DATE_OF_BIRTH:
+        if (date_of_birth.DATE):
+            date_aliases.add(date_of_birth.DATE)
+
     for alias in individual.INDIVIDUAL_ALIAS:
         if alias.QUALITY == 'Low':
             continue  # skip these for now
@@ -42,6 +51,10 @@ for individual in individuals:
         alias_names = " ".join(alias_names.split())  # remove white-space and linebreaks
         if alias_names:
             alias_list = alias_names.split(";")
-            for item in alias_list: aliases.add(item)
+            for item in alias_list: name_aliases.add(item)
 
-    print(fixedRef, aliases)
+    if len(date_aliases) > 1:
+        multiple_dates = multiple_dates + 1
+    print(fixedRef, name_aliases, [d.strftime("%Y-%m-%d") for d in date_aliases])
+
+print("Found", multiple_dates, "entries with multiple exact dates", 100.0*multiple_dates/len(individuals), "%")
