0258f700a7480415752ff2a47f4f7c7bf1112ac9
Per Henden <per.christian.henden@eika.no>
2018-08-28 09:16:39 +0200

    Prettify

--- create_noiseword_list.py
+++ create_noiseword_list.py
@@ -1,3 +1,7 @@
+# Prints the most common words in the sanction lists.
+# Manually inspect the lists to find noise words (stop words), like "The", "abd", and other words
+# that do not contribute to search results.
+
 from eu import reader as eu_reader
 from ofac import reader as ofac_reader
 from un import reader as un_reader
@@ -5,7 +9,17 @@ from collections import Counter
 from normalizer import normalize_aliases
 
 
-def find_most_common_words(sanction_entries):
+def print_word_frequencies(stop_words, stop_words_short, key):
+    print("\nNormal length", key, ":")
+    for (word, count) in stop_words:
+        print(word, count)
+
+    print("\nShort length", key, ":")
+    for (word, count) in stop_words_short:
+        print(word, count)
+
+
+def find_most_common_words(sanction_entries, count):
     words = []
     short_words = []
     for sanction_entry in sanction_entries:
@@ -20,18 +34,13 @@ def find_most_common_words(sanction_entries):
             else:
                 words.append(name_part)
 
-    different_words = set(words)
-    different_shortwords = set(short_words)
-    stopword_count = int(15 * len(words) / len(different_words))  # heuristic
-    stopword_count_short_words = int(20 * len(short_words) / len(different_shortwords))  # heuristic
-
     word_counter = Counter()
     short_word_counter = Counter()
 
     word_counter.update(list(words))
     short_word_counter.update(list(short_words))
-    stop_words = [word[0] for word in word_counter.most_common(stopword_count)]
-    stop_words_short = [word[0] for word in short_word_counter.most_common(stopword_count_short_words)]
+    stop_words = word_counter.most_common(count)
+    stop_words_short = short_word_counter.most_common(count)
     return (stop_words, stop_words_short)
 
 
@@ -54,14 +63,12 @@ for item in persons.items(): all_persons.append(item)
 for item in entities.items(): all_entities.append(item)
 for item in persons.items(): all_persons.append(item)
 
+count = 100
 print("Loaded {} entities and {} persons from sanction lists".format(len(all_entities), len(all_persons)))
+print(count, "Most commons words are:\n")
 
-(stop_words, stop_words_short) = find_most_common_words(all_persons)
-print("Most common words for individuals:")
-print("Normal length:", stop_words)
-print("Short length:", stop_words_short)
+(stop_words, stop_words_short) = find_most_common_words(all_persons, count)
+print_word_frequencies(stop_words, stop_words_short, "individuals")
 
-(stop_words, stop_words_short) = find_most_common_words(all_entities)
-print("\nMost common words for entities:")
-print("Normal length:", stop_words)
-print("Short length:", stop_words_short)
+(stop_words, stop_words_short) = find_most_common_words(all_entities, count)
+print_word_frequencies(stop_words, stop_words_short, "entities")
